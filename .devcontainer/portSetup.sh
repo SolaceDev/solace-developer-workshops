8ee9b901e5389b2ba61340ef3080ad933d10b415
Tamimi <tamimi@solace.com>
2025-03-05 08:16:30 -0800

    update config

--- .devcontainer/portSetup.sh
+++ .devcontainer/portSetup.sh
@@ -1,4 +1,15 @@
-echo "Exposing port 8080"
-gh codespace ports visibility 8080:public -c $CODESPACE_NAME
-gh codespace ports visibility 1443:public -c $CODESPACE_NAME
-gh codespace ports visibility 8008:public -c $CODESPACE_NAME
+#!/bin/bash
+
+# Array of ports to make public
+PORTS=(8080 1443 8008 55555 1883 8000 5672 9000 2223)
+
+# Function to set port visibility
+set_port_visibility() {
+    echo "Exposing port $1"
+    gh codespace ports visibility "$1":public -c "$CODESPACE_NAME"
+}
+
+# Loop through ports and set visibility
+for port in "${PORTS[@]}"; do
+    set_port_visibility "$port"
+done
