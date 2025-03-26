8986f73923f09f2b9a9c70f312b90010c86f9a46
Tamimi <tamimi@solace.com>
2025-03-25 22:02:20 -0700

    comment solace install

--- .devcontainer/configureEnv.sh
+++ .devcontainer/configureEnv.sh
@@ -20,5 +20,5 @@ while (! docker info > /dev/null 2>&1); do
   sleep 2
 done
 
-# Install the Solace image
-docker run -d -p 8080:8080 -p 55555:55555  -p 1443:1443 -p 8008:8008 -p 1883:1883 -p 8000:8000 -p 5672:5672 -p 9000:9000 -p 2223:2222 --shm-size=2g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --name=solace solace/solace-pubsub-standard
+# # Install the Solace image
+# docker run -d -p 8080:8080 -p 55555:55555  -p 1443:1443 -p 8008:8008 -p 1883:1883 -p 8000:8000 -p 5672:5672 -p 9000:9000 -p 2223:2222 --shm-size=2g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --name=solace solace/solace-pubsub-standard
