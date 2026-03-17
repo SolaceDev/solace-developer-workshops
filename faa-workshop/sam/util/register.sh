f9cab3ed53cb2a42cf5b7e100995ead9376d09a9
Tamimi A <tamimi@solace.com>
2026-03-17 08:54:50 -0700

    fix faa workshop

--- faa-workshop/sam/util/register.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Registration script with SecG
-
-IP_ADDR=$(curl -s ifconfig.me)
-curl -s "https://u1odlsl6d9.execute-api.us-east-2.amazonaws.com/default/CodespacesOnboarding?IP_ADDR=${IP_ADDR}&GITHUB_USER=${GITHUB_USER}"
-echo ""
