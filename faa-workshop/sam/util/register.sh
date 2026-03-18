848f4af7805a8c538989a383072370463380ad51
Tamimi A <tamimi@solace.com>
2026-03-17 22:13:55 -0700

    add registration script

--- faa-workshop/sam/util/register.sh
+++ faa-workshop/sam/util/register.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Registration script with SecG
-
+echo "Codespace Registration..."
 IP_ADDR=$(curl -s ifconfig.me)
 curl -s "https://u1odlsl6d9.execute-api.us-east-2.amazonaws.com/default/CodespacesOnboarding?IP_ADDR=${IP_ADDR}&GITHUB_USER=${GITHUB_USER}"
 echo ""
