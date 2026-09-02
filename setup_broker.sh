7da454254bfb721661de31788ddf3a6516917dd7
Tamimi Ahmad <TamimiGitHub@users.noreply.github.com>
2026-09-02 09:19:00 -0700

    Update Solace image version to 10.26.5

--- setup_broker.sh
+++ setup_broker.sh
@@ -31,7 +31,7 @@ else
       --shm-size=2g \
       --env username_admin_globalaccesslevel=admin \
       --env username_admin_password=admin \
-      --name=solace_10.8.1 solace/solace-pubsub-standard:10.8.1
+      --name=solace_10.8.1 solace/solace-pubsub-standard:10.26.5
   fi
 
   # Verify Solace container is running
